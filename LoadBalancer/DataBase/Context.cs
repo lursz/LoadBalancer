b4b5337a5796400e292808f21572c49d3ddb8dbc
lursz <lukoszr@gmail.com>
2023-11-20 21:56:27 +0100

    Interceptor endeavour

--- LoadBalancer/DataBase/Context.cs
+++ LoadBalancer/DataBase/Context.cs
@@ -25,6 +25,7 @@ public class Context : DbContext
     {
         Console.WriteLine("OnConfiguring");
         optionsBuilder.UseNpgsql(_connectionString);
+        optionsBuilder.AddInterceptors(new TaggedQueryCommandInterceptor());
     }
 
     // protected override void OnModelCreating(ModelBuilder modelBuilder)
--- LoadBalancer/Program.cs
+++ LoadBalancer/Program.cs
@@ -10,6 +10,10 @@ internal static class Program
     private static void Main(string[] args)
     {
         // DbHandler.Create(new User { Id = 5, Name = "Ana", Email = "ana@gmail.com", Sex = "Other" });
-        DbHandler.Delete(new User { Id = 5 });
+        // DbHandler.Delete(new User { Id = 5 });
+
+        var context = new Context("Host=localhost;Port=5432;Username=user1;Password=password1;Database=database1");
+        context.User.Add(new User { Id = 6, Name = "Ana", Email = "ana@gmail.com", Sex = "Other" });
+        context.SaveChanges();
     }
 }
