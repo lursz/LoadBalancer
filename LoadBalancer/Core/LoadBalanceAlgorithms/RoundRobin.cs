7266a0e685a6cd60f211612025f7b94717cc3546
lursz <lukoszr@gmail.com>
2023-12-24 01:24:31 +0100

    RoundRobin correction

--- LoadBalancer/Core/LoadBalanceAlgorithms/RoundRobin.cs
+++ LoadBalancer/Core/LoadBalanceAlgorithms/RoundRobin.cs
@@ -3,12 +3,17 @@ using LoadBalancer.Abstracts;
 namespace LoadBalancer.Core.LoadBalanceAlgorithms;
 public class RoundRobin : ILoadBalanceAlgorithm
 {
+    private int _index = 0;
     public DatabaseSession chooseSession(DatabaseSession[] sessions)
     {
-        foreach (var session in sessions)
+        var attempts = sessions.Length;
+        while (attempts > 0)
         {
+            var session = sessions[_index];
+            _index = (_index + 1) % sessions.Length;
             if (session.Status == ManageableSession.Status.UP && session.isUsed() == false)
                 return session;
+            attempts--;
         }
         throw new InvalidOperationException("RoundRobin: Failed to choose a suitable session.");
     }
