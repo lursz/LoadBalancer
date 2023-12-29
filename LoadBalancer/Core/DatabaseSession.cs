c59e81b2de4f4f763691b32d7c1c383abe11ca80
Maciej Adamus <adamus.mac@gmail.com>
2023-12-29 21:48:43 +0100

    started implementing DatabaseSession

--- LoadBalancer/Core/DatabaseSession.cs
+++ LoadBalancer/Core/DatabaseSession.cs
@@ -1,18 +1,88 @@
+using System.Runtime.CompilerServices;
 using LoadBalancer.Abstracts;
+using LoadBalancer.Connection;
+using NHibernate;
+using NHibernate.Cfg;
 
 namespace LoadBalancer.Core;
 
 public class DatabaseSession : ManageableSession
 {
-    public DatabaseSession()
+    private LoadBalancerInterceptor interceptor;
+    private string connectionString;
+    private string name;
+    private Status status;
+    private LinkedList<DbRequest> queue;
+    private ISession session;
+    private ISession interceptedSession;
+    
+    public DatabaseSession(LoadBalancerInterceptor interceptor, DbInstance dbInstance)
     {
-        Console.WriteLine("DatabaseSession created");
+        
+        this.interceptor = interceptor;
+        this.connectionString = dbInstance.ConnectionString;
+        this.name = dbInstance.Name;
+        try
+        {
+            Connect();
+            this.status = Status.UP;
+        }
+        catch (Exception e)
+        {
+            this.status = Status.DOWN;
+            Console.WriteLine($"[NHIBERNATE SESSION '{name}'] Could not create connection. Details: {e.Message}");
+        }
+        this.queue = new LinkedList<DbRequest>();
+    }
+    
+    private void Connect()
+    {
+        var configuration = new Configuration();
+        configuration.Configure(this.connectionString);
+        var sessionFactory = configuration.BuildSessionFactory();
+        this.session = sessionFactory.OpenSession();
+        this.interceptedSession = sessionFactory.WithOptions().Interceptor(interceptor).OpenSession();
     }
 
-    public override object execute()
+    public override object execute(DbRequest request)
     {
-        Console.WriteLine("DatabaseSession execute");
-        return null;
+        try
+        {
+            if (!session.Transaction.IsActive)
+                session.BeginTransaction();
+
+            switch (request.getType())
+            {
+                case DbRequest.Type.INSERT:
+                    session.Save(request.getObject());
+                    break;
+                case DbRequest.Type.UPDATE:
+                    session.Update(request.getObject());
+                    break;
+                case DbRequest.Type.DELETE:
+                    session.Delete(request.getObject());
+                    break;
+                default:
+                    throw new NotSupportedException($"Operation '{request.getType()}' is not supported");
+            }
+            
+            // session.Transaction.Commit();
+            session.GetCurrentTransaction().Commit();
+            session.Clear();
+            return null;
+        }
+        catch (NotSupportedException exception)
+        {
+            status = Status.DOWN;
+            register(request);
+            Console.WriteLine($"[NHIBERNATE SESSION '{name}'] Could not execute request. Details: {exception.Message}");
+            throw;
+        }
+    }
+    
+    private void register(DbRequest request)
+    {
+        queue.AddLast(request);
     }
 
     public override void fix()
---
+++ LoadBalancer/Core/DbRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace LoadBalancer.Core;
+
+public class DbRequest
+{
+    public enum Type
+    {
+        SELECT, INSERT, UPDATE, DELETE
+    }
+    
+    private readonly object dbObject;
+    private readonly Type type;
+    
+    public DbRequest(object dbObject, Type type)
+    {
+        this.dbObject = dbObject;
+        this.type = type;
+    }
+    
+    public object getObject()
+    {
+        return this.dbObject;
+    }
+    
+    public Type getType()
+    {
+        return this.type;
+    }
+}
