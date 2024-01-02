c141d0370a3e95a37b3f76f54981ba593c18b12b
lursz <lukoszr@gmail.com>
2024-01-02 02:11:01 +0100

    Improvments to LoadBalanceAlgorithms
    Both RoundRobin and Random will now cycle through Sessions until they find suitable one

--- LoadBalancer/Core/LoadBalanceAlgorithms/Random.cs
+++ LoadBalancer/Core/LoadBalanceAlgorithms/Random.cs
@@ -4,17 +4,29 @@ namespace LoadBalancer.Core.LoadBalanceAlgorithms;
 
 public class Random<Session> : ILoadBalanceAlgorithm<Session> where Session : ManageableSession
 {
-    private readonly int _allowedAttempts = 10;
+    private readonly int _allowedAttempts = 2;
     public Session chooseSession(Session[] sessions)
     {
-        var attempts = _allowedAttempts;
-        while (attempts > 0)
+        Session[] sessionsCopy = new Session[sessions.Length];
+        sessions.CopyTo(sessionsCopy, 0);
+        
+        var randomIndex = new System.Random().Next(0, sessionsCopy.Length);
+        for (int i = randomIndex; i < sessionsCopy.Length; i++)
         {
-            var session = sessions[0];
-            if (session.status == ManageableSession.Status.UP && session.isUsed == false)
-                return session;
-            attempts--;
+            if (sessionsCopy[i] == null)
+                continue;
+            
+            var session = sessionsCopy[i];
+            var attempts = _allowedAttempts;
+            while (attempts > 0)
+            {
+                if (session.status == ManageableSession.Status.UP && session.isUsed == false)
+                    return session;
+                attempts--;
+            }
+            sessionsCopy[i] = null;
         }
+        
         throw new InvalidOperationException("Random: Failed to choose a suitable session.");
     }
 }
--- LoadBalancer/Core/LoadBalanceAlgorithms/RoundRobin.cs
+++ LoadBalancer/Core/LoadBalanceAlgorithms/RoundRobin.cs
@@ -4,16 +4,19 @@ namespace LoadBalancer.Core.LoadBalanceAlgorithms;
 public class RoundRobin<Session> : ILoadBalanceAlgorithm<Session> where Session : ManageableSession
 {
     private int _index = 0;
+    private readonly int _allowedAttempts = 3;
     public Session chooseSession(Session[] sessions)
     {
-        var attempts = sessions.Length;
-        while (attempts > 0)
+        var attempts = _allowedAttempts;
+        for (int i = _index % sessions.Length; i < sessions.Length; i++)
         {
-            var session = sessions[_index];
-            _index = (_index + 1) % sessions.Length;
-            if (session.status == ManageableSession.Status.UP && session.isUsed == false)
-                return session;
-            attempts--;
+            var session = sessions[i];
+            while (attempts > 0)
+            {
+                if (session.status == ManageableSession.Status.UP && session.isUsed == false)
+                    return session;
+                attempts--;
+            }
         }
         throw new InvalidOperationException("RoundRobin: Failed to choose a suitable session.");
     }
